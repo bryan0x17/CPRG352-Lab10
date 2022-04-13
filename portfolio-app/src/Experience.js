465f7eea3bdd5aed53998affd2afe312777ae6c3
Bryan <50989230+bryan0x17@users.noreply.github.com>
2022-04-12 18:44:25 -0600

    Update Experience.js

--- portfolio-app/src/Experience.js
+++ portfolio-app/src/Experience.js
@@ -1,39 +1,31 @@
 function Experience() {
-    return (
-        <div className="card">
-            <div className="card-body">
-                <h2 className="card-title">Work Experience</h2>
-                <ul className="list-unstyled">
-                    <li>
-                        <h6 className="fw-bold">Senior Web Developer / Digital Agency 2016-2020</h6>
-                        <p>Phasellus et tellus iaculis, interdum augue vel, luctus nulla. Aenean viverra,
-                            magna
-                            a
-                            ultricies
-                            elementum, dui mi tristique ligula, non euismod leo mauris ac metus.</p>
-                    </li>
-                    <li>
-                        <h6 className="fw-bold">Web Developer / Big Firm 2010-2015</h6>
-                        <p>Mauris volutpat, ex condimentum fringilla imperdiet, nulla turpis semper diam, ac
-                            semper
-                            risus urna quis
-                            libero. Fusce non nunc quam. Ut vulputate, magna eget molestie luctus, elit est
-                            posuere
-                            lorem, quis
-                            dapibus dolor felis id mi.</p>
-                    </li>
-                    <li>
-                        <h6 className="fw-bold">Junior Web Developer / Websites 'R' Us 2004-2009</h6>
-                        <p>Sed eu turpis placerat, posuere odio a, viverra velit. Pellentesque habitant
-                            morbi
-                            tristique
-                            senectus et
-                            netus et malesuada fames ac turpis egestas.</p>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h2 className="card-title">Work Experience</h2>
+        <ul className="list-unstyled">
+          <li>
+            <h6 className="fw-bold">IT Intern Sep. 2021 - Dec. 2021</h6>
+            <p>
+              Assisted with the installation and testing of a new security
+              software suite on servers and endpoints.
+            </p>
+            <p>
+              Monitored backups for successful execution.
+            <p>
+            Extended Nagios monitoring to additional servers and services.
+            </p>
+            Wrote a custom Nagios plugin in Python to monitor servers.
+            <p>
+            Wrote and modified scripts for performing tasks and monitoring
+            servers.
+            </p>
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Experience;
